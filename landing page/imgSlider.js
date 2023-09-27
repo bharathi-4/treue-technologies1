d6ee7b791ae95a42d11e3eb1715aec7931383b1a
bharathi-4 <146207893+bharathi-4@users.noreply.github.com>
2023-09-27 11:23:03 -0700

    Add files via upload
    Landing page

---
+++ landing page/imgSlider.js
@@ -0,0 +1,7 @@
+function imgSlider(anything) {
+    document.querySelector('.starbucks').src = anything;
+}
+function changeBgcolor(color) {
+    const sec = document.querySelector('.sci');
+    sec.style.background = color;
+}
